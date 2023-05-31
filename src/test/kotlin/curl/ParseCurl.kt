9acf88b94a08bd2dcaafe4072428524f169200b9
David Wickes <dave.wickes@gmail.com>
2023-05-31 17:33:40 +0100

    handling quotes because I was worried about quotes

--- src/test/kotlin/curl/ParseCurl.kt
+++ src/test/kotlin/curl/ParseCurl.kt
@@ -17,12 +17,39 @@ class ParseCurlTest {
 
         assertEquals(expected, request)
     }
+
+    @Test
+    fun `does the right thing with double quotes`() {
+        val curl = "curl \"http://gypsydave5.com\""
+
+        val request = Request.parseCurl(curl)
+
+        val expected = Request(Method.GET, "http://gypsydave5.com")
+
+        assertEquals(expected, request)
+    }
+
+    @Test
+    fun `does the right thing with single quotes`() {
+        val curl = "curl 'http://gypsydave5.com'"
+
+        val request = Request.parseCurl(curl)
+
+        val expected = Request(Method.GET, "http://gypsydave5.com")
+
+        assertEquals(expected, request)
+    }
 }
 
 private fun Request.Companion.parseCurl(curl: String): Request {
     var tokens = curl.split(" ")
     if (tokens.first() == "curl") {
         tokens = tokens.drop(1)
+    }
+    tokens = tokens.map {
+        it.trim()
+            .removeSurrounding("\"")
+            .removeSurrounding("'")
     }
 
     return Request(Method.GET, tokens.last())
