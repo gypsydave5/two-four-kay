6cdd02a55984c2d2e0173694c35084038986c21f
David Wickes <dave.wickes@gmail.com>
2023-06-01 09:23:55 +0100

    using a scanner now - but should I be using ANTLR?

--- NOTES.md
+++ NOTES.md
@@ -4,3 +4,10 @@ handling HTTP/1.1 HTTP/2 differences
 - may be useful https://superuser.com/questions/1042053/how-to-make-firefox-use-http-1-1
 
 Need to handle the $s in headers, queries, body strings
+
+## cURL
+
+Should I use ANTLR to build a lexer/parser or just
+hand-roll it myself from the bottom up?
+
+https://tomassetti.me/antlr-mega-tutorial/
--- src/test/kotlin/curl/ParseCurl.kt
+++ src/test/kotlin/curl/ParseCurl.kt
@@ -2,6 +2,8 @@ package curl
 
 import org.http4k.core.Method
 import org.http4k.core.Request
+import org.http4k.core.Uri
+import java.util.*
 import kotlin.test.Test
 import kotlin.test.assertEquals
 
@@ -39,18 +41,49 @@ class ParseCurlTest {
 
         assertEquals(expected, request)
     }
+
+    @Test
+    fun `can parse the method`() {
+        val curl = "curl -X POST 'http://gypsydave5.com'"
+
+        val request = Request.parseCurl(curl)
+
+        val expected = Request(Method.POST, "http://gypsydave5.com")
+
+        assertEquals(expected, request)
+    }
 }
 
 private fun Request.Companion.parseCurl(curl: String): Request {
-    var tokens = curl.split(" ")
-    if (tokens.first() == "curl") {
-        tokens = tokens.drop(1)
-    }
-    tokens = tokens.map {
-        it.trim()
-            .removeSurrounding("\"")
-            .removeSurrounding("'")
+    val input = Scanner(curl).useDelimiter(" ")
+    var request = Request(Method.GET, "")
+
+    if (input.hasNext("curl")) {
+        input.next()
     }
 
-    return Request(Method.GET, tokens.last())
+    while (input.hasNext()) {
+        if (input.hasNext("-.*")) {
+            request = parseFlag(input, request)
+        }
+
+        request = parseUri(input, request)
+    }
+
+
+
+    return request
+}
+
+private fun String.removeSurroundingQuotes() = trim()
+    .removeSurrounding("\"")
+    .removeSurrounding("'")
+
+fun parseFlag(input: Scanner, request: Request): Request {
+    println(">>>>>" + input.next())
+    return request.method(Method.valueOf(input.next().removeSurroundingQuotes()))
+}
+
+fun parseUri(input: Scanner, request: Request): Request {
+    return request.uri(Uri.of(input.next().removeSurroundingQuotes()))
 }
